b9ba0b11cbae9f0a7e9ace4aeca11cf9cf24852e
David Lin <yoxisem544@gmail.com>
2016-04-17 21:01:18 +0800

    Initial Commit

---
+++ PercentageTranition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PercentageTranition
+//
+//  Created by David on 2016/4/17.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
